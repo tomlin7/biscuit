960b0fa4da95fade5c3379eb4a70cbd71e4093f4
billyeatcookies <billydevbusiness@gmail.com>
2021-10-05 02:49:29 -0500

    load config data

--- src/lib/base.py
+++ src/lib/base.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
+import lib.config as config
+
 from lib.editor import Editor
 from lib.statusbar import SLabel, SButton, StatusBar
 
 class Base:
     def __init__(self, root, *args, **kwargs):
         self.root = root
+
+        self.config = config.Config()
+        self.bindings = config.Bindings()
 
         self.editor = Editor(self.root)
         self.statusbar = StatusBar(self.root)
--- src/lib/config/__init__.py
+++ src/lib/config/__init__.py
@@ -1,2 +1,29 @@
+import json
+
+from lib.config.bindings import Bindings
+from lib.config.theme import Theme
+
+
 class Config:
-    pass
+    theme: str
+    font: dict
+
+    # "theme": "default",
+    # "font": {
+    #     "family": "Consolas", 
+    #     "size": 16,
+    #     "style": "normal"
+    # }
+
+    
+    def __init__(self):
+        self.config = None
+
+    def load_data(self):
+        with open('config/settings.json') as settingsfile:
+            self.config = json.load(settingsfile)
+        self.assign_data()
+    
+    def assign_data(self):
+        self.theme = self.config['theme']
+        self.font = self.config['font']
