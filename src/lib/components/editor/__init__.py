cfd4d79fa8f4359e561904267590d4d0d0d33bfa
billyeatcookies <billydevbusiness@gmail.com>
2021-10-10 05:52:39 -0500

    Line numbers! and scrollbar

--- src/lib/components/editor/__init__.py
+++ src/lib/components/editor/__init__.py
@@ -1,13 +1,28 @@
+import tkinter as tk
+
 from lib.components.text import Text
+from lib.components.editor.utils.linenumbers import LineNumbers
 from lib.components.editor.utils.binder import Binder
 
-class Editor(Text):
+class Editor(tk.Frame):
     def __init__(self, master, path, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
 
-        self.path = path
-        self.load_file(path=self.path)
+        self.text = Text(master=self, path=path)
+        self.linenumbers = LineNumbers(master=self, text=self.text)
 
-        self.configure(font=self.base.settings.font)
+        self.scrollbar = tk.Scrollbar(self, orient="vertical", command=self.text.yview)
+        self.text.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.linenumbers.pack(side=tk.LEFT, fill=tk.Y)
+        self.text.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        
+        self.text.bind("<<Change>>", self._on_change)
+        self.text.bind("<Configure>", self._on_change)
+        
         # self.binder = Binder(bindings=self.base.settings.bindings, editor=self)
+
+    def _on_change(self, event):
+        self.linenumbers.redraw()
