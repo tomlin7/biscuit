970c66f3e6bb66af9e668181396bd4157ce6c2eb
billyeatcookies <billydevbusiness@gmail.com>
2021-10-26 23:14:36 -0500

    Git window class

--- src/lib/components/git/__init__.py
+++ src/lib/components/git/__init__.py
@@ -1,2 +1,22 @@
+import tkinter as tk
+
 from lib.components.git.core import GitCore
 from lib.components.git.repo import GitRepo
+
+
+# TODO: change into git pane later on
+class GitWindow(tk.Toplevel):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.base = master
+        
+        self.core = self.base.git
+        self.init_window()
+        self.init_widgets()
+
+    def init_window(self):
+        self.title("Bisgit")
+        self.geometry("400x400")
+
+    def init_widgets(self):
+        self.lbl = tk.Label(self, text=f"branch: {self.core.get_active_branch()}")
