aced10e4b284091ba8a34bb2114866d33ef66db7
billyeatcookies <billydevbusiness@gmail.com>
2021-10-27 01:24:45 -0500

    include utils

--- src/lib/components/git/__init__.py
+++ src/lib/components/git/__init__.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from lib.components.git.core import GitCore
 from lib.components.git.repo import GitRepo
 
+from lib.components.text import utils
 
 # TODO: change into git pane later on
 class GitWindow(tk.Toplevel):
