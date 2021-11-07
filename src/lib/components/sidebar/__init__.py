749d7310f3113e7520d385f6abdd59b7122a14f8
billyeatcookies <billydevbusiness@gmail.com>
2021-11-07 11:08:28 -0600

    Improved sidebar

--- src/lib/components/sidebar/__init__.py
+++ src/lib/components/sidebar/__init__.py
@@ -4,14 +4,20 @@ import tkinter as tk
 class Sidebar(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
+        self.base = master.base
 
-        btn1 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="A", font=("Consolas", 10), bg="#F8B195")
-        btn1.pack(fill=tk.X, side=tk.TOP)
-        btn2 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="B", font=("Consolas", 10), bg="#F67280")
-        btn2.pack(fill=tk.X, side=tk.TOP)
-        btn3 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="C", font=("Consolas", 10), bg="#C06C84")
-        btn3.pack(fill=tk.X, side=tk.TOP)
-        btn4 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="D", font=("Consolas", 10), bg="#6C5878")
-        btn4.pack(fill=tk.X, side=tk.TOP)
-        btn5 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="E", font=("Consolas", 10), bg="#355C7D")
-        btn5.pack(fill=tk.X, side=tk.TOP)
+        self.config(width=50, bg='#FFFFFF', relief=tk.FLAT, borderwidth=2)
+
+        btn1 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="A", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn1.pack(fill=tk.X, side=tk.TOP) #, pady=1)
+        btn2 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="B", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn2.pack(fill=tk.X, side=tk.TOP) #, pady=1)
+        btn3 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="C", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn3.pack(fill=tk.X, side=tk.TOP) #, pady=1)
+        btn4 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="D", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn4.pack(fill=tk.X, side=tk.TOP) #, pady=1)
+        btn5 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="E", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn5.pack(fill=tk.X, side=tk.TOP) #, pady=1)
+
+        btn6 = tk.Menubutton(self, height=3, width=6, relief=tk.FLAT, text="F", font=("Consolas", 10), bg="#DEDDDD", fg="#000000", activebackground="#A9A9A9", activeforeground="#45494c")
+        btn6.pack(fill=tk.X, side=tk.BOTTOM)
