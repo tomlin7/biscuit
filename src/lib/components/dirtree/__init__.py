e4f78660f89f39bcc768b5878fbf3a656d6d5d2a
billyeatcookies <billydevbusiness@gmail.com>
2021-11-08 05:17:34 -0600

    Restore Explorer label in dirtreepane

--- src/lib/components/dirtree/__init__.py
+++ src/lib/components/dirtree/__init__.py
@@ -9,14 +9,17 @@ class DirTreePane(SidePane):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
 
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        self.label = tk.Label(self, text="Explorer", anchor=tk.W, padx=10, pady=10)
+        self.label.grid(row=0, column=0, sticky=tk.EW)
+
         self.tree = DirTreeTree(self, selectmode=tk.BROWSE)
-        self.tree.grid(row=0, column=0, sticky=tk.NSEW)
+        self.tree.grid(row=1, column=0, sticky=tk.NSEW)
 
         self.tree_scrollbar = AutoScrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
-        self.tree_scrollbar.grid(row=0, column=1, sticky=tk.NS)
+        self.tree_scrollbar.grid(row=1, column=1, sticky=tk.NS)
         
         self.tree.configure(yscrollcommand=self.tree_scrollbar.set)
     
