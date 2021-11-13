a568c67f3c148d7a3956b4b804cd9ce5be756212
billyeatcookies <billydevbusiness@gmail.com>
2021-11-12 23:34:16 -0600

    Scrolling two diff panes simultaneously

--- src/lib/components/diff_viewer/content.py
+++ src/lib/components/diff_viewer/content.py
@@ -15,10 +15,24 @@ class DiffViewerContent(ttk.PanedWindow):
 
         self.lhs_frame = DiffViewerPane(self, path)
         self.add(self.lhs_frame, weight=1)
-        self.lhs_frame.load_text("previous")
+        self.lhs_frame.load_file()
 
         self.rhs_frame = DiffViewerPane(self, path)  # count lines for this later
         self.add(self.rhs_frame, weight=1)
         self.rhs_frame.load_file()
 
         self.text = self.lhs_frame.content.text
+
+        self.lhs_frame.content.scrollbar['command'] = self.on_scrollbar
+        self.rhs_frame.content.scrollbar['command'] = self.on_scrollbar
+        self.lhs_frame.content.text['yscrollcommand'] = self.on_textscroll
+        self.rhs_frame.content.text['yscrollcommand'] = self.on_textscroll
+
+    def on_scrollbar(self, *args):
+        self.lhs_frame.content.text.yview(*args)
+        self.rhs_frame.content.text.yview(*args)
+
+    def on_textscroll(self, *args):
+        self.lhs_frame.content.scrollbar.set(*args)
+        self.rhs_frame.content.scrollbar.set(*args)
+        self.on_scrollbar('moveto', args[0])
