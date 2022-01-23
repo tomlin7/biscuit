40ab2c70162a589f121308bb7e5faf5a6dbd3440
billyeatcookies <billydevbusiness@gmail.com>
2022-01-23 06:32:09 -0600

    Dirtree toolbar funtions

--- src/lib/components/dirtree/utils/toolbar.py
+++ src/lib/components/dirtree/utils/toolbar.py
@@ -9,29 +9,33 @@ class DirTreeToolbar(tk.Frame):
 
         self.dirvar = tk.StringVar()
         self.dirvar.set('NO FOLDER OPENED')
-        
+
         self.dirname = tk.Label(self)
         self.dirname.config(
-            font=("Helvetica", 11, 'bold'), anchor=tk.W, 
+            font=("Helvetica", 11, 'bold'), anchor=tk.W,
             textvariable=self.dirvar, bg="#E6E6E6")
-        
-        self.collapse_all = tk.Menubutton(
+
+        self.btn_collapse_all = tk.Menubutton(
             self, text="–", fg="#000000", font=("Helvetica", 11), width=2, bg="#E6E6E6",
-            activebackground="#4c4a48", activeforeground="#ffffff") # , command=self.refresh)
+            activebackground="#4c4a48", activeforeground="#ffffff")
 
-        self.refresh = tk.Menubutton(
+        self.btn_refresh = tk.Menubutton(
             self, text="⟳", fg="#000000", font=("Helvetica", 11), width=2, bg="#E6E6E6",
-            activebackground="#4c4a48", activeforeground="#ffffff") # , command=self.refresh)
+            activebackground="#4c4a48", activeforeground="#ffffff")
 
-        self.newfile = tk.Menubutton(
+        self.btn_newfile = tk.Menubutton(
             self, text="+", fg="#000000", font=("Helvetica", 11), width=2, bg="#E6E6E6",
-            activebackground="#4c4a48", activeforeground="#ffffff") #, command=self.newfile)
+            activebackground="#4c4a48", activeforeground="#ffffff")
 
         self.dirname.pack(side=tk.LEFT, padx=(25, 0))
-        self.collapse_all.pack(side=tk.RIGHT)
-        self.refresh.pack(side=tk.RIGHT)
-        self.newfile.pack(side=tk.RIGHT)
+        self.btn_collapse_all.pack(side=tk.RIGHT)
+        self.btn_refresh.pack(side=tk.RIGHT)
+        self.btn_newfile.pack(side=tk.RIGHT)
 
     def update_dirname(self):
         if self.base.active_dir_name:
             self.dirvar.set(self.base.active_dir_name.upper())
+
+    def newfile(self): ...
+    def refresh(self): ...
+    def collapse_all(self): ...
