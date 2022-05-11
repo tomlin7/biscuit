a5d84e5caf30d7da6c59447a7298cba4c38d008c
Billy <billydevbusiness@gmail.com>
2022-05-11 12:37:06 +0300

    Turn sysinfo to dataclass

--- src/core/utils/sysinfo.py
+++ src/core/utils/sysinfo.py
@@ -1,8 +1,10 @@
 import sys
 import platform
 
+from dataclasses import dataclass
 
-class SysInfo:
+
+class SysInfo(dataclass):
     os: None
     version: None
     release: None
@@ -19,3 +21,11 @@ class SysInfo:
         self.machine = platform.machine()
         self.processor = platform.processor()
         self.python_version = sys.version
+
+    def __str__(self):
+        return f"""
+        OS: {self.os} ({self.version}, {self.release})
+        Machine: {self.machine}
+        Processor: {self.processor}
+        Python Version: {self.python_version}
+        """
