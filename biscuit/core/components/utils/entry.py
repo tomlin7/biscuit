732c4596e91f249e00b07a3b3d794ce1007b0504
Billy <billydevbusiness@gmail.com>
2023-06-28 01:47:58 +0800

    Checking the webhook - hello tkinter community <3

---
+++ biscuit/core/components/utils/entry.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+from hintedtext import HintedEntry
+
+
+class Entry(tk.Frame):
+    def __init__(self, master, hint="", *args, **kwargs):
+        super().__init__(master)
+        self.master = master
+        self.base = master.base
+        
+        self.config(padx=1, pady=1, bg='#dfdfdf')
+        self.grid_columnconfigure(0, weight=1)
+
+        self.entry = HintedEntry(self, relief=tk.FLAT, bd=5, hint=hint)
+        self.entry.config(*args, **kwargs)
+        self.entry.grid(row=0, column=0, sticky=tk.NSEW)
+
+    def insert(self, *args):
+        self.entry.insert(*args)
