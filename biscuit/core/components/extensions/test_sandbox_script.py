308e59e30b24c7195945f610b05bc1865cfc6852
Billy <billydevbusiness@gmail.com>
2023-07-10 04:28:10 +0800

    feat: Diffeditor modified words in lines are highlighted too

--- biscuit/core/components/extensions/test_sandbox_script.py
+++ biscuit/core/components/extensions/test_sandbox_script.py
@@ -6,6 +6,5 @@ def run():
     print("Extension is running!")
     print("Square root of 25:", math.sqrt(25))
     print("Random number:", random.randint(1, 10))
-    print("Accessing restricted module (should raise an error):")
     import os  # Restricted module
-    print(os.getcwd())  # This line won't be executed
+    print("Haha i just bypassed your security *evil laugh*", os.getcwd())  # This line won't be executed
