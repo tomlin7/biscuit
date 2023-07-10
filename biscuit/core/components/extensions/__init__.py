21a3f5bfe6ebf40c946ee11ce579f61852dfe49f
Billy <billydevbusiness@gmail.com>
2023-07-11 02:34:53 +0800

    feat: Extensions - blacklisted imports instead of whitelist - reimplemented

--- biscuit/core/components/extensions/__init__.py
+++ biscuit/core/components/extensions/__init__.py
@@ -5,36 +5,44 @@ import os, threading, sys
 class ExtensionManager:
     def __init__(self, base):
         self.base = base
-        self.extensions = []
+        self.extensions = {}
 
-        self.allowed_modules = ['_io', 'math', 'random', 'tkinter', 'toml', 'json', '']
-        self.allowed_imports = {module: __import__(module) for module in self.allowed_modules}
+        #TODO file watcher for extensions directory
+
+        self.blocked_modules = ['os', 'sys']
+        self.imports = {module: __import__(module) for module in sys.builtin_module_names}
         sys.modules['builtins'].__import__ = self.restricted_import
 
         self.load_extensions()
 
     def restricted_import(self, name, globals={}, locals={}, fromlist=[], level=0):
-        if name in self.allowed_modules:
-            return self.allowed_imports[name]
-    
-        raise ImportError("Module '{}' is not allowed.".format(name))
+        if name in self.blocked_modules:
+            raise ImportError("Module '{}' is not allowed.".format(name))
+
+        return self.imports[name]
 
     def load_extensions(self):
-        "currently loads all extension in the directory"
+        #TODO support loading/running/unloading individual extensions
         extension_files = os.listdir(self.base.extensionsdir)
         for extension_file in extension_files:
             if extension_file.endswith(".py"):
                 extension_name = os.path.splitext(extension_file)[0]
-                module_name = f"extensions.{extension_name}"
-                try:
-                    extension_module = importlib.import_module(module_name)
-                    extension_instance = extension_module.Extension(self.base.api)
-                    self.extensions.append(extension_instance)
+                if extension_name in self.extensions.keys():
+                    return
 
-                    self.base.logger.info(f"Extension '{extension_name}' loaded.")
-                except ImportError as e:
-                    self.base.logger.error(f"Failed to load extension '{extension_name}': {e}")
-                    self.base.notifications.error(f"Extension '{extension_name}' failed: see logs.")
+                self.load_extension(extension_name)
+
+    def load_extension(self, extension_name):
+        module_name = f"extensions.{extension_name}"
+        try:
+            extension_module = importlib.import_module(module_name)
+            extension_instance = extension_module.Extension(self.base.api)
+            self.extensions[extension_name] = extension_instance
+
+            self.base.logger.info(f"Extension '{extension_name}' loaded.")
+        except ImportError as e:
+            self.base.logger.error(f"Failed to load extension '{extension_name}': {e}")
+            self.base.notifications.error(f"Extension '{extension_name}' failed: see logs.")
     
     def start_server(self):
         self.base.logger.info(f"Extensions server started.")
@@ -46,7 +54,7 @@ class ExtensionManager:
         self.server.join()
 
     def run_extensions(self):
-        for extension in self.extensions:
+        for _, extension in self.extensions.items():
             try:
                 extension.run()
             except Exception as e:
