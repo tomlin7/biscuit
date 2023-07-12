be3a380fd0734ed05a0b879080741663abc19fb3
Billy <billydevbusiness@gmail.com>
2023-07-12 17:06:38 +0800

    fix: Scaling tk based on screen DPI

--- biscuit/app.py
+++ biscuit/app.py
@@ -60,9 +60,9 @@ class App(tk.Tk):
     
     def setup(self):
         """Sets up the Tkinter window, path, and configurations of the application."""
+        self.setup_tk()
         self.setup_path()
         self.setup_configs()
-        self.setup_tk()
 
     def late_setup(self):
         """Sets up the references, binds, and extensions of the application."""
@@ -73,6 +73,22 @@ class App(tk.Tk):
         self.palette.register_actionset(lambda: self.settings.actionset)
         self.setup_extensions()
     
+    def setup_tk(self):
+        """Sets up the Tkinter window size, title, and minimum size."""
+        windll.shcore.SetProcessDpiAwareness(1)
+
+        self.dpi_value = self.winfo_fpixels('1i')
+        self.scale = self.dpi_value / 65
+        self.tk.call('tk', 'scaling', '-displayof', '.', self.scale)
+
+        app_width = round(1000 * self.scale)
+        app_height = round(650 * self.scale)
+        # x = int((self.winfo_screenwidth() - app_width) / 2)
+        # y = int((self.winfo_screenheight() - app_height) / 2)
+
+        self.geometry(f"{app_width}x{app_height}")
+        self.title("Biscuit")
+        
     def setup_path(self):       
         """Sets up the application directory, configuration directory, resource directory, and extensions directory."""
         self.appdir = os.path.dirname(__file__)
@@ -115,35 +131,6 @@ class App(tk.Tk):
         self.source_control = self.root.baseframe.sidebar.source_control
         self.logger = self.panel.logger
 
-    def setup_tk(self):
-        """Sets up the Tkinter window size, title, and minimum size."""
-        windll.shcore.SetProcessDpiAwareness(1)
-
-        app_width = 1500
-        app_height = 950
-        # x = int((self.winfo_screenwidth() - app_width) / 2)
-        # y = int((self.winfo_screenheight() - app_height) / 2)
-
-        self.geometry(f"{app_width}x{app_height}")
-        self.minsize(800, 600)
-        self.title("Biscuit")
-    
-    def setup_tk(self):
-        """Sets up the Tkinter window size, title, and minimum size."""
-        windll.shcore.SetProcessDpiAwareness(1)
-
-        self.dpi_value = self.winfo_fpixels('1i')
-        self.scale = self.dpi_value / 65
-        self.tk.call('tk', 'scaling', '-displayof', '.', self.scale)
-
-        app_width = round(1000 * self.scale)
-        app_height = round(650 * self.scale)
-        # x = int((self.winfo_screenwidth() - app_width) / 2)
-        # y = int((self.winfo_screenheight() - app_height) / 2)
-
-        self.geometry(f"{app_width}x{app_height}")
-
-        self.title("Biscuit")
 
     def setup_extensions(self):
         """Sets up the extensions API and manager of the application."""
