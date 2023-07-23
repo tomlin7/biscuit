ee0eb3b83bc8cf9cf531de23d5eaf5f3bed9cb8b
Billy <billydevbusiness@gmail.com>
2023-07-23 20:55:38 +0800

    build: Nightly build configurations

--- biscuit/__init__.py
+++ biscuit/__init__.py
@@ -1,9 +1,10 @@
-__version__ = '2.11.0'
+__version__ = '2.20.0'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
 # For tests to run successfully
 import sys
 from os.path import abspath, dirname, join
+
 sys.path.append(abspath(join(dirname(__file__), '.')))
 
 
