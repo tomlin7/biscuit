b9bd8f37897d9acd971bf75084f80c5c2537abb2
Billy <billydevbusiness@gmail.com>
2023-07-25 14:43:37 +0800

    fix: Make splashscreen windows specific

--- biscuit/__main__.py
+++ biscuit/__main__.py
@@ -5,14 +5,13 @@ To run the app, do:
 >>> python -m biscuit
 """
 
-try:
+import platform, sys
+
+# The splash screen (windows specific)
+if platform.os == "Windows" and getattr(sys, 'frozen', False):
     import pyi_splash
     pyi_splash.update_text("Initializing...")
     pyi_splash.close()
-except:
-    pass
-
-import sys
 
 from biscuit.app import App
 
