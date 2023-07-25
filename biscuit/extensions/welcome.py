ba1589685b78368aeace3735c90b863de5ca9f74
Billy <billydevbusiness@gmail.com>
2023-07-25 17:34:14 +0800

    test: Make tests pass (pytest)

--- biscuit/extensions/welcome.py
+++ biscuit/extensions/welcome.py
@@ -3,8 +3,6 @@
 class Extension:
     def __init__(self, api):
         self.api = api
-        self.api.notifications.show(f"Welcome to Biscuit")
 
     def run(self):
-        ...
-        
+        self.api.notifications.show(f"Welcome to Dev mode!")
