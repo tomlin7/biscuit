ddbce2974c2c60a0eb38555f20cfcd8a87c9ebae
cid0rz <cid.kampeador@gmail.com>
2023-08-05 14:06:17 +0200

    wip small improvements

--- biscuit/core/components/games/stackengineer.py
+++ biscuit/core/components/games/stackengineer.py
@@ -12,6 +12,25 @@ from biscuit.core.components.editors.texteditor import TextEditor
 from biscuit.core.components.games.game import BaseGame
 from biscuit.core.components.editors.languages import Languages
 
+
+
+HELP_TEXT = """Stack Engineer is a small coding game where you have to manage data to achieve objectives.
+The machine has 3 registers:
+-NR: Number register. It manages numerical values to do operations and to get and put values to stacks.
+-TR: Text register. It manages strings to do operations on them and to get and put strings to stacks.
+-TEST: Test register. it is a register that stores the result of the last comparison executed and when it is TRUE
+       it executes the following jump instruction.
+The machine has always one stack initialized (S1) and can have more. These initialized stacks will have relevant
+data to complete the exercises. The user can add stacks on demand.
+
+The syntax of the language is as follows:
+
+
+
+
+"""
+
+
 class TestLabel(tk.Label):
 
     def __init__(self, parent, *args, textvariable=None,  **kwargs):
@@ -114,6 +133,8 @@ class StackEngineer(BaseGame):
         self.step_once_btn.grid(row=1,column=0)
         self.run_program_btn = tk.Button(self.buttons,text="Run", command=self.run_program)
         self.run_program_btn.grid(row=2, column=0)
+        self.reset_program_btn = tk.Button(self.buttons, text="Reset program", command=self.reset_program)
+        self.reset_program_btn.grid(row=3, column=0)
 
         self.registers = tk.Frame(self)
         self.registers.grid(row=3, column=0, sticky="ew")
@@ -131,7 +152,7 @@ class StackEngineer(BaseGame):
         self.test_text.grid(row=1, column=2 , padx=(5,5))
         self.test_register.set("FALSE")
 
-        #print(se.m_stacks['S1'].pop())
+        #print(self.stacks['S1'].pop())
         #self.stacks['S1'].insert(10, 1)      
                 
 
@@ -157,15 +178,19 @@ class StackEngineer(BaseGame):
         if self.line_pos > self.get_last_line():
             self.end_round()
         else:
-            linestart = str(self.line_pos) + ".0"
-            self.editor.linenumbers.remove_mark()
-            self.editor.linenumbers.mark_line(linestart, marker="->")
+            self.mark_line(self.line_pos)
             
 
     def get_last_line(self):
         last_line_index = self.editor.text.index("end - 1 line")
         last_line = int(last_line_index.split('.')[0])
         return last_line
+
+    def mark_line(self, line, marker="->"):
+        linestart = str(line) + ".0"
+        self.editor.linenumbers.remove_mark()
+        self.editor.linenumbers.mark_line(linestart, marker="->")
+        
 
     def run_program(self):
         """Execute the program till the end"""
@@ -186,12 +211,29 @@ class StackEngineer(BaseGame):
         next_btn = tk.Button(dialog, text="Next Level", font=dialog_font)
         next_btn.grid(row=1, column=1)
 
+    def show_error(self, error_msg="No error msg provided."):
+        """Show an error when a problem is encountered and offer to restart the execution"""
+        error = tk.Toplevel(self)
+        error.title("Error in code")
+        error_lbl= tk.Label(error, text=f"Error in line {self.line_pos}: {error_msg} ")
+        error_lbl.pack()
+
+    def reset_program(self):
+        """Restart program execution"""
+        self.labels = {}
+        self.line_pos = 1
+        self.mark_line(self.line_pos)
+        self.number_register.set(0)
+        self.text_register.set("")
+        
+
     def start_level(self, level=1):
         """Set up the solution and initial conditions, reset cost"""
         self.cost = 0
         self.labels = {}
         self.line_pos = 1
-        self.editor.linenumbers.mark_line("1.0", marker="->")
+        self.mark_line(self.line_pos)
+        #load solution
 
     def interpret_line(self, line):
         """get a line of code an process it"""
@@ -199,7 +241,7 @@ class StackEngineer(BaseGame):
             self.line_pos +=1
             return
         line = line.split("#",1)[0]
-        linelist = line.split()
+        linelist = line.upper().split()
         
         print(linelist)
         if len(linelist) == 0:
@@ -207,7 +249,44 @@ class StackEngineer(BaseGame):
         elif len(linelist) == 1 and linelist[0][-1] == ":":
             self.labels[linelist[0][:-1]] = self.line_pos
             print(self.labels)
+        elif len(linelist) == 3:
+            inst = linelist[0] #command to execute
+            if inst == "PUSH":
+                stack_n = linelist[2] #stack to push to
+                reg = linelist[1] # register to push from
+                if stack_n not in self.stacks:
+                    self.show_error(error_msg=f"Stack {stack_n} not available, you can create it and restart the game")
+                else:
+                    if reg not in ['NR', 'TR']:
+                        self.show_error(error_msg=f"{linelist[1]} is not a valid register")
+                    elif reg == 'NR':
+                        val = self.number_register.get()
+                        self.stacks[stack_n].push(val)
+                    elif reg == 'TR':
+                        val = self.text_register.get()
+                        self.stacks[stack_n].push(val)
 
+            elif inst == "POP":
+                stack_n = linelist[1] #stack to pop from
+                if stack_n not in self.stacks:
+                    self.show_error(error_msg=f"{stack_n} is not a valid stack")
+                else:
+                    val = self.stacks[linelist[1]].pop()
+                    print(val, type(val))
+                    self.number_register.set(val)
+
+        elif len(linelist) == 2:
+            inst = linelist[0] #command to execute
+            if inst == "ADD":
+                val = linelist[1] #thing to add
+                print(val, type(val))
+            elif inst == "SUB":
+                pass
+            elif inst == "MUL":
+                pass
+            elif inst == "DIV":
+                pass
+            
         self.line_pos += 1
 
         
