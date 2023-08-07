a301e596efd450d401f1e6df4a1c7d8a61c9f785
Billy <billydevbusiness@gmail.com>
2023-08-08 05:50:13 +0800

    chore: Bump version to `2.20.5` (nightly)

--- biscuit/__init__.py
+++ biscuit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.20.3'
+__version__ = '2.20.5'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
 # For tests to run successfully
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ from biscuit import __version__
 
 
 def test_version():
-    assert __version__ == '2.20.3'
+    assert __version__ == '2.20.5'
