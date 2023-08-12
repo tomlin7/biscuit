948ddc05467fd6dcccc998403d1f3b9333677303
cid0rz <cid.kampeador@gmail.com>
2023-08-12 14:30:04 +0200

    improve stack engineer again

--- biscuit/core/components/games/stackengineer.py
+++ biscuit/core/components/games/stackengineer.py
@@ -4,6 +4,8 @@ version = "0.1"
 import random
 import tkinter as tk
 from collections import Counter, deque
+from functools import partial
+import re
 from tkinter import messagebox, ttk
 
 
@@ -24,9 +26,62 @@ The machine has always one stack initialized (S1) and can have more. These initi
 data to complete the exercises. The user can add stacks on demand.
 
 The syntax of the language is as follows:
+*<SX> = the stack to be operated [S1, S2, ...], in case * is used before the stack name, the stack is only read and the value persists in the stack after the operation.
+<RX> = The register to be operated [NR, TR]
+<INT> = an integer [...,20,...,25,...]
+<TX> = a string ["a string"]
+<LB> = a user created label
 
+POP <SX> <R>
+    pop the X stack head into the designated register.
+    example: POP S3 NR
 
+PUSH <R> <SX>
+    push the designated register into X stack.
+    example: PUSH NR S2
 
+SWP <SX> *<SY>/<INT>
+    swap the values in stack X from the head to designated index.
+    example: SWP S2 *S3
+
+ADD *<SX>/<INT>
+    add an integer to the Number Register
+    example: ADD *S3
+
+SUB *<SX>/<INT>
+    substract an integer from the Number Register
+    example: SUB S3
+
+MUL *<SX>/<INT>
+    multiply the Number Register by an integer
+    example: MUL *S2
+
+DIV *<SX>/<INT>
+    divide the Number Register by an integer
+    example: DIV S1
+
+CCL/CCR *<SX>/<TX>
+    concatenate a string to the Text Register on the left/right
+    example: CCL "HELLO"
+
+RCL/RCR *<SX>/<INT>
+    remove some characters from the left/right of the text register
+    example: RCL 2
+
+TEQ/TGT/TGE/TLT/TLE *<SX>/<INT>
+    test if the Number Register is equal/greater/greater_or_equal
+    smaller/smaller_or_equal compared to a number
+    example: TEQ 10
+
+JMP *<SX>/<INT>/<LB>
+    jumps to the provided line or label
+    example: JMP start
+
+CLNR
+    sets the Number Register to 0
+
+CLTR
+    sets the Text Register empty
 
 """
 
@@ -106,7 +161,15 @@ class Stack(ttk.LabelFrame):
         self.tw.delete(*self.tw.get_children())
         for i,val in enumerate(self.values):
             self.tw.insert("", index=i, iid=str(i), values=(i,val))
-
+    def swap(self, pos):
+        new_head = self.read(pos)
+        old_head = self.pop()
+        self.push(new_head)
+        self.values[pos] = old_head
+        self.update()
+    def __len__(self):
+        return len(self.values)
+        
         
 class StackEngineer(BaseGame):
     name = f"Stack Engineer v{version}"
@@ -121,6 +184,7 @@ class StackEngineer(BaseGame):
         self.level = 1
         self.stack_id = 0
         self.line_pos = 1
+        self.error = False
         self.number_register = tk.IntVar()
         self.text_register = tk.StringVar()
         self.test_register = tk.StringVar()
@@ -202,7 +266,7 @@ class StackEngineer(BaseGame):
 
     def run_program(self):
         """Execute the program till the end"""
-        while self.line_pos <= self.get_last_line():
+        while self.line_pos <= self.get_last_line() and not self.error:
             self.step_once()
 
     def end_round(self):
@@ -224,12 +288,18 @@ class StackEngineer(BaseGame):
         error = tk.Toplevel(self)
         error.title("Error in code")
         error_lbl= tk.Label(error, text=f"Error in line {self.line_pos}: {error_msg} ")
-        error_lbl.pack()
+        error_lbl.grid(row=0, column=0)
+        error_restart_btn = tk.Button(error, text="Restart program", command=partial(self.reset_program, dialog=error))
+        error_restart_btn.grid(row=1, column=0)
+        self.error = True
 
-    def reset_program(self):
+    def reset_program(self, dialog=None):
         """Restart program execution"""
+        if dialog:
+            dialog.destroy()
         self.labels = {}
         self.line_pos = 1
+        self.error = False
         self.mark_line(self.line_pos)
         self.number_register.set(0)
         self.text_register.set("")
@@ -292,17 +362,38 @@ class StackEngineer(BaseGame):
                             val = int(val)
                             self.number_register.set(val)
                         except ValueError as ve:
-                            self.show_error("Value must be a number for the number register")
+                            self.show_error(error_msg="Value must be a number for the number register")
                     elif reg == "TR":
                         self.text_register.set(val)
+
+            elif inst == "SWP":
+                stack_n = linelist[1] #stack to operate in
+                try:
+                    idx = int(linelist[2]) #index to swap
+                except ValueError as ve:
+                    self.show_error(error_msg=str(ve))
+    
+                if stack_n not in self.stacks:
+                    self.show_error(error_msg=f"Stack {stack_n} not available, you can create it and restart the game")
+                else:
+                    st = self.stacks[stack_n]
+                    if len(st) < idx or idx < 0:
+                        self.show_error(error_msg=f"index {idx} not found in stack {stack_n}")
+                    st.swap(idx)
+                
+                
+            else:
+                self.show_error(error_msg=f"Incorrect number of parameters for funcion {inst}")
 
         elif len(linelist) == 2:
             inst = linelist[0] #command to execute
             val = linelist[1] #argument of the command
             operand = None
-            if val.startswith("*S"):
+            read_pattern = r'^\*?S\d+$'
+            pop_pattern =  r'^S\d+$'
+            if re.match(read_pattern, val):
                 operand = self.stacks[val[1:]].read(0)
-            elif val.startswith("S"):
+            elif re.match(pop_pattern, val):
                 operand = self.stacks[val].pop()
             elif val in self.labels:
                 operand = self.labels[val]
@@ -316,7 +407,7 @@ class StackEngineer(BaseGame):
                     nr = int(self.number_register.get()) + operand
                     self.number_register.set(nr)
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
                     
             elif inst == "SUB":
                 #substract a number from the NR register
@@ -325,7 +416,7 @@ class StackEngineer(BaseGame):
                     nr = int(self.number_register.get()) - operand
                     self.number_register.set(nr)
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
                 
             elif inst == "MUL":
                 #multiply the NR register by a number
@@ -334,7 +425,7 @@ class StackEngineer(BaseGame):
                     nr = int(self.number_register.get()) * operand
                     self.number_register.set(nr)
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
                     
             elif inst == "DIV":
                 #perform integer division of the NR register by a number
@@ -343,12 +434,12 @@ class StackEngineer(BaseGame):
                     nr = int(self.number_register.get()) // operand
                     self.number_register.set(nr)
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
 
             elif inst == "CCL":
-                self.text_register = str(operand) + self.text_register
+                self.text_register.set(str(operand).strip('"') + self.text_register.get())
             elif inst == "CCR":
-                self.text_register = self.text_register + str(operand)
+                self.text_register.set(self.text_register.get() + str(operand).strip('"'))
             
             elif inst == "TEQ":
                 try:
@@ -357,7 +448,7 @@ class StackEngineer(BaseGame):
                     if tst:
                         self.test_register.set('TRUE')
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
             elif inst == "TGT":
                 try:
                     operand = int(operand)
@@ -365,7 +456,7 @@ class StackEngineer(BaseGame):
                     if tst:
                         self.test_register.set('TRUE')
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
             elif inst == "TGE":
                 try:
                     operand = int(operand)
@@ -373,7 +464,7 @@ class StackEngineer(BaseGame):
                     if tst:
                         self.test_register.set('TRUE')
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
             elif inst == "TLT":
                 
                 try:
@@ -382,7 +473,7 @@ class StackEngineer(BaseGame):
                     if tst:
                         self.test_register.set('TRUE')
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
 
             elif inst == "TLE":
                  try:
@@ -391,7 +482,7 @@ class StackEngineer(BaseGame):
                     if tst:
                         self.test_register.set('TRUE')
                  except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
             elif inst == "JMP":
                 if self.test_register.get() != 'TRUE':
                     self.line_pos += 1
@@ -404,18 +495,33 @@ class StackEngineer(BaseGame):
                     self.mark_line(self.line_pos)
                     self.test_register.set('FALSE')
                 except ValueError as ve:
-                    self.show_error(str(ve))
+                    self.show_error(error_msg=str(ve))
+
+            elif inst == "RCL":
+                try:
+                    operand = int(operand)
+                    self.text_register.set(self.text_register.get()[operand:])
+                except ValueError as ve:
+                    self.show_error(error_msg=str(ve))
+                
+            elif inst == "RCR":
+                try:
+                    operand = int(operand)
+                    self.text_register.set(self.text_register.get()[:-operand])
+                except ValueError as ve:
+                    self.show_error(error_msg=str(ve))
+            else:
+                self.show_error(error_msg=f"Incorrect number of parameters for funcion {inst}")
 
         elif len(linelist) == 1:
             inst = linelist[0]
-            if inst == "RCL":
-                self.text_register = self.text_register[1:]
-            elif inst == "RCR":
-                self.text_register = self.text_register[:-1]
-            elif inst == "CLNR":
+
+            if inst == "CLNR":
                 self.number_register.set(0)
             elif inst == "CLTR":
                 self.text_register.set("")
+            else:
+                self.show_error(error_msg=f"Incorrect number of parameters for funcion {inst}")
             
         self.line_pos += 1
 
