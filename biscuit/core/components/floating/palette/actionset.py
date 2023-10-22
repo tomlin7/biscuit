9476433f9ff1b352fd1c9c713f9bf441ddfd6f11
Dheeraj Charaungonath <144347155+billyowo@users.noreply.github.com>
2023-10-22 18:35:48 +0800

    docs: Update Actionset docs

--- biscuit/core/components/floating/palette/actionset.py
+++ biscuit/core/components/floating/palette/actionset.py
@@ -1,16 +1,37 @@
-class ActionSet(list):
-    def __init__(self, description, prompt, items=[], pinned=[], *args, **kwargs) -> None:
-        super().__init__(items, *args, **kwargs)
-        self.description = description
-        self.prompt = prompt
+from typing import Callable, List, Tuple
 
-        self.pinned = pinned # [[command, callback], ...]
+
+class ActionSet(list):
+    def __init__(self, description: str, prompt: str, items: List[Tuple[str, Callable]] = [], 
+                    pinned: List[Tuple[str, Callable]] = [], *args, **kwargs) -> None:
+        """Palette Actionset
+        A list of items that can be searched through.
+        
+        Attributes
+        ----------
+        description : str
+            The description of the actionset.
+        prompt : str
+            The prompt of the actionset.
+        items : List[Tuple[str, Callable]]
+            The items in the actionset.
+        pinned : List[Tuple[str, Callable]]
+            The pinned items in the actionset.
+        """
+        
+        super().__init__(items, *args, **kwargs)
+        self.description: str = description
+        self.prompt: str = prompt
+
+        self.pinned: List[Tuple[str, Callable]] = pinned # [[command, callback], ...]
     
     def update(self, items) -> None:
+        "Clear and update the items in the actionset."
         self.clear()
         self += items
     
     def get_pinned(self, term) -> list:
+        "Returns the pinned items with the term formatted."
         if not self.pinned:
             return []
         
