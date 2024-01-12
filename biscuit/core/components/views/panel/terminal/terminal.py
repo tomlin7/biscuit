f49bd3e35872f0a0f4e0bbdc6e2ff91c69f39019
Billy <billydevbusiness@gmail.com>
2024-01-13 04:35:21 +0530

    feat: Pseudoterminals (use pywinpty) partial implementation

--- biscuit/core/components/views/panel/terminal/terminal.py
+++ biscuit/core/components/views/panel/terminal/terminal.py
@@ -1,13 +1,17 @@
-import queue
-import subprocess
 import tkinter as tk
 from threading import Thread
+
+from requests import head
+
+from .ansi import replace_newline, strip_ansi_escape_sequences
 
 from biscuit.core.components.utils import Scrollbar
 
 from ..panelview import PanelView
 from .text import TerminalText
-
+from winpty import PTY
+# from pty import PtyProcess
+# from ptyprocess import PtyProcess
 
 class Terminal(PanelView):
     """
@@ -26,6 +30,8 @@ class Terminal(PanelView):
     """
     name: str
     icon: str
+    shell: str
+    p: PTY
 
     def __init__(self, master, cwd=".", *args, **kwargs) -> None:
         super().__init__(master, *args, **kwargs)
@@ -37,93 +43,72 @@ class Terminal(PanelView):
         self.alive = False
         self.cwd = cwd
 
-        self.terminal = TerminalText(self, relief=tk.FLAT, padx=10, pady=10, font=("Consolas", 11))
-        self.terminal.grid(row=0, column=0, sticky=tk.NSEW)
-        self.terminal.bind("<Return>", self.enter)
+        self.text = TerminalText(self, relief=tk.FLAT, padx=10, pady=10, font=("Consolas", 11))
+        self.text.grid(row=0, column=0, sticky=tk.NSEW)
+        self.text.bind("<Return>", self.enter)
 
         self.terminal_scrollbar = Scrollbar(self, style="EditorScrollbar")
         self.terminal_scrollbar.grid(row=0, column=1, sticky='NSW')
 
-        self.terminal.config(yscrollcommand=self.terminal_scrollbar.set)
-        self.terminal_scrollbar.config(command=self.terminal.yview, orient=tk.VERTICAL)
+        self.text.config(yscrollcommand=self.terminal_scrollbar.set)
+        self.terminal_scrollbar.config(command=self.text.yview, orient=tk.VERTICAL)
 
-        self.terminal.tag_config("prompt", foreground=self.base.theme.biscuit_dark)
-        self.terminal.tag_config("command", foreground=self.base.theme.biscuit)
+        self.text.tag_config("prompt", foreground=self.base.theme.biscuit_dark)
+        self.text.tag_config("command", foreground=self.base.theme.biscuit)
 
+        self.bind("<Destroy>", self.destroy)
+    
     def start_service(self, *_) -> None:
         self.alive = True
         self.last_command = None
+        
+        self.p = PTY(80, 25)
+        self.p.spawn(self.shell)
 
-        self.p = subprocess.Popen(
-            self.shell, stdout=subprocess.PIPE, cwd=self.cwd,
-            stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        self.out_queue = queue.Queue()
-        self.err_queue = queue.Queue()
-
-        self.t_out = t_out = Thread(target=self.process_out)
-        self.t_err = t_err = Thread(target=self.process_err)
-        t_out.daemon = True
-        t_err.daemon = True
-        t_out.start()
-        t_err.start()
-
-        self.show_prompt()
-        self.write_loop()
-
-        self.bind("<Destroy>", self.destroy)
-
-    def show_prompt(self) -> None:
-        if self.base.sysinfo.os == "Linux":
-            self.write("->>", "prompt")
+        Thread(target=self.write_loop, daemon=True).start()
 
     def destroy(self, *_) -> None:
         self.alive = False
-        self.p.kill()
-        super().destroy()
 
     def run_command(self, command) -> None:
-        self.write(command, "command")
+        self.insert(command, "command")
         self.enter()
 
     def enter(self, *_) -> None:
-        command = self.terminal.get('input', 'end')
+        command = self.text.get('input', 'end')
         self.last_command = command
-        self.terminal.register_history(command)
+        self.text.register_history(command)
+        if command.strip():
+            self.text.delete('input', 'end')
 
-        self.p.stdin.write(command.encode())
-        self.p.stdin.flush()
-
-    def process_out(self) -> None:
-        while self.alive:
-            data = self.p.stdout.raw.read(1024)
-            self.out_queue.put(data)
-
-    def process_err(self) -> None:
-        while self.alive:
-            data = self.p.stderr.raw.read(1024)
-            self.err_queue.put(data)
+        self.p.write(command + "\r\n")
+        return "break"
 
     def write_loop(self) -> None:
-        """ write data from stdout and stderr to the Text widget"""
-        if not self.err_queue.empty():
-            self.write(self.err_queue.get())
-            self.show_prompt()
-        if not self.out_queue.empty():
-            if self.last_command:
-                self.write(self.out_queue.get()[len(self.last_command)-1:].rstrip())
-                self.last_command = None
-            else:
-                self.write(self.out_queue.get())
-            self.show_prompt()
-        if self.alive:
-            self.after(10, self.write_loop)
-
-    def write(self, output, tag=None) -> None:
-        self.terminal.insert(tk.END, output, tag)
+        while self.alive:
+            if buf := self.p.read():
+                p = buf.find('\x1b]0;')
+                
+                if p != -1:
+                    buf = buf[:p]
+                buf = [strip_ansi_escape_sequences(i) for i in replace_newline(buf).splitlines()]
+                self.insert('\n'.join(buf))
+                
+    def insert(self, output, tag=None) -> None:
+        self.text.insert(tk.END, output, tag)
         #self.terminal.tag_add("prompt", "insert linestart", "insert")
-        self.terminal.see(tk.END)
-        self.terminal.mark_set('input', 'insert')
+        self.text.see(tk.END)
+        self.text.mark_set('input', 'insert')
+    
+    def newline(self):
+        self.insert('\n')
 
     def clear(self) -> None:
-        self.terminal.clear()
+        self.text.clear()
+
+    def ctrl_key(self, key) -> None:
+        if key == 'c':
+            self.run_command('\x03')
+            
+    def __str__(self) -> str:
+        return self.name
