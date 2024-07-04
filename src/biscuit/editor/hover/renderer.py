c16e0d55c2e6ebfd6b5f2e7f2aee0a0bc2f283ca
Billy <billydevbusiness@gmail.com>
2024-07-04 18:16:21 +0900

    feat: Added code block syntax highlighting in hover symbol info

--- src/biscuit/editor/hover/renderer.py
+++ src/biscuit/editor/hover/renderer.py
@@ -1,12 +1,74 @@
 from __future__ import annotations
 
+import tkinter as tk
 import typing
 
 import mistune
+from mistune import InlineParser
+from mistune.plugins.abbr import abbr
+from mistune.plugins.def_list import def_list
+from mistune.plugins.footnotes import footnotes
+from mistune.plugins.formatting import strikethrough
+from mistune.plugins.speedup import speedup
+from mistune.plugins.table import table
+from mistune.plugins.task_lists import task_lists
+from mistune.plugins.url import url
+from pygments import highlight
+from pygments.formatters import html
+from pygments.lexers import get_lexer_by_name
+from pygments.style import Style
 from tkinterweb import HtmlFrame
 
+from biscuit.common.ui import Frame, Scrollbar
+
 if typing.TYPE_CHECKING:
-    from . import Hover
+    from ...settings.config import Theme
+    from .hover import Hover
+
+
+class HighlightRenderer(mistune.HTMLRenderer):
+    def __init__(self, style, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.style = style
+        self.formatter = html.HtmlFormatter(style=self.style)
+
+    def block_code(self, code, info=None):
+        try:
+            if info:
+                lexer = get_lexer_by_name(info, stripall=True)
+                return highlight(code, lexer, self.formatter)
+        except Exception as e:
+            print(e)
+
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
+
+
+class BiscuitStyle(Style):
+    def __init__(self, theme: Theme):
+        super().__init__()
+        self.theme = theme
+        self.styles = self.theme.syntax
+
+    def __iter__(self):
+        for token, color in self.theme.syntax.items():
+            if isinstance(color, dict):
+                yield token, {
+                    "color": color["foreground"][1:],
+                    "bold": False,
+                    "italic": False,
+                    "underline": False,
+                    "bgcolor": None,
+                    "border": None,
+                }
+            else:
+                yield token, {
+                    "color": color[1:],
+                    "bold": False,
+                    "italic": False,
+                    "underline": False,
+                    "bgcolor": None,
+                    "border": None,
+                }
 
 
 class HoverRenderer(HtmlFrame):
@@ -17,11 +79,29 @@ class HoverRenderer(HtmlFrame):
         self.base = master.base
         self.html.shrink(True)
 
-    def render_markdown(self, rawmd):
-        self.load_html(mistune.html(rawmd))
+        self.style = BiscuitStyle(self.base.theme)
+
+        self.renderer = HighlightRenderer(self.style, escape=False)
+        self.formatter = self.renderer.formatter
+        self.markdown = mistune.Markdown(
+            renderer=self.renderer,
+            inline=InlineParser(),
+            plugins=[
+                abbr,
+                def_list,
+                footnotes,
+                strikethrough,
+                speedup,
+                table,
+                task_lists,
+                url,
+            ],
+        )
+
         t = self.base.theme
-        self.add_css(
-            f"""
+        pygments_css = self.formatter.get_style_defs(".highlight")
+        self.css = f"""
+            {pygments_css}
             CODE, PRE {{
                 font-family: {self.base.settings.font['family']};
                 font-size: {self.base.settings.font['size']}pt;
@@ -31,6 +111,7 @@ class HoverRenderer(HtmlFrame):
             BODY {{
                 background-color: {t.secondary_background};
                 color: {t.secondary_foreground};
+                align-items: left;
             }}
             img {{
                 max-width: 100%;
@@ -45,6 +126,11 @@ class HoverRenderer(HtmlFrame):
             li{{
                 margin-left:1px;
             }}
+
+            tr, th, td {{
+                border: 1px solid {t.border};
+            }}
+
             :link    {{ color: {t.biscuit}; }}
             :visited {{ color: {t.biscuit_dark}; }}
             INPUT, TEXTAREA, SELECT, BUTTON {{ 
@@ -57,5 +143,8 @@ class HoverRenderer(HtmlFrame):
                 color: tcl(::tkhtml::if_disabled {t.primary_background}{t.primary_foreground_highlight});
             }}
             """
-        )
+
+    def render_markdown(self, rawmd):
+        self.load_html(self.markdown(rawmd))
+        self.add_css(self.css)
         self.update_idletasks()
