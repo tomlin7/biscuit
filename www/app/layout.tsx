96c03f85ed25e7da938c2b21f047a55890d0ef8d
Tomlin7 <billydevbusiness@gmail.com>
2026-07-29 17:43:51 +0530

    Remove Fumadocs from www, populate Mintlify docs with comprehensive Biscuit documentation

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,4 +1,3 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 
@@ -6,11 +5,11 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider theme={{ defaultTheme: 'dark' }}>{children}</RootProvider>
+        {children}
       </body>
     </html>
   );
