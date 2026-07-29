304727730c62f84fda3b74bd558f2d0e48c622ef
Tomlin7 <billydevbusiness@gmail.com>
2026-07-29 17:52:38 +0530

    Redirect /docs to https://docs.biscuit.tomlin7.com/

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/docs/:path*',
+        destination: 'https://docs.biscuit.tomlin7.com/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default config;
